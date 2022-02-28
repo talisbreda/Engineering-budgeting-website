6ba8a226dba0f99b66738c4b1436540547660a16
talisbreda <talis.breda@outlook.com>
2022-02-28 13:16:09 -0300

    Added comments to specific sections of edit.ts

--- source/front/edit.ts
+++ source/front/edit.ts
@@ -2,9 +2,12 @@ import 'regenerator-runtime/runtime';
 import axios from 'axios';
 
 const base_url = 'http://localhost:1337';
+
+/** Uses the cookies created on the home page to get the IDs of the user and the project */
 const idUsuario:any = document.cookie.split('user=')[1].split(';')[0]
 const idProjeto:any = document.cookie.split('projectID=')[1].split(';')[0]
 
+/** Assigns a variable to each of the inputs on the right side of the page */
 var qtd_areia: any = document.querySelector('#qtd_areia')
 var qtd_cimento: any = document.querySelector('#qtd_cimento')
 var qtd_tijolo: any = document.querySelector('#qtd_tijolo')
@@ -22,35 +25,45 @@ var custo_telha: any = document.querySelector('#custo_telha')
 var custo_tinta: any = document.querySelector('#custo_tinta')
 var custo_total: any = document.querySelector('#custo_total')
 
-const form:any = document.querySelector('form')
-
+/** Creating objects containing the project and user's IDs, used for calling API routes later */
 const todoUser = {
     id_usuario: idUsuario
 }
-
 const todoProject = {
     id_projeto: idProjeto
 }
 
 var input_data: any = document.querySelectorAll('.inputData')
 
+/** This function is called when the page is loaded. It looks for a specific cookie to check 
+ *  if the loaded project is newly created or if it already existed */
 window.onload = async function() {
     if (document.cookie.includes('new=true')) {
+        /** If the project is newly created, calls the API to get a list of projects from the user
+         *  and takes the ID of the last one created*/
         const responseSelect = await axios.post(`${base_url}/get/projects`, todoUser);
         const selectData = responseSelect.data.projects
         const project_id = selectData[selectData.length - 1].id_projeto
 
+        /** Puts the project ID into a cookie */
         document.cookie = `projectID=${project_id}`;        
     } else {
+        /** If the project already existed previously, calls fillData() to fill the left side inputs
+         *  and then fillOutput() to fill the inputs on the right side */
         await fillData()
         await fillOutput()
     }
 }
 
 document.querySelectorAll('.title').forEach(expandCollapse)
+/** This function handles the expanding and collapsing of the blocks when clicking the titles */
 async function expandCollapse(title:any) {
+    /** Taking the number on the id of the caller element and searching for a block with the same
+     *  number on the id */
     const element_id = title.id.split('title')[1]
     const block:any = document.querySelector(`#input${element_id}`)
+
+    /** Checks if the element is expanded or collapsed and changes its state */
     title.addEventListener('click', (event:Event) => {
         if (block.style.display == 'none') {
             block.style.display = 'block'
@@ -60,12 +73,14 @@ async function expandCollapse(title:any) {
     })
 }
 
-
+/** This function is responsible for filling the inputs with the data acquired from the detabase */
 async function fillData() {
+    /** Calling the API that returns an object and converts this object into an array */
     const responseProject = await axios.post(`${base_url}/get/project`, todoProject);
     const responseData = responseProject.data
     const projectData = Object.values(responseData.projects[0])
 
+    /** Tries to insert the data from the database to the inputs */
     try {
         for (var i = 0; i < input_data.length; i++) {
             input_data[i].value = projectData[i]
@@ -76,12 +91,17 @@ async function fillData() {
     }
 }   
 
+const form:any = document.querySelector('form')
 form.addEventListener('submit', async (event: Event) => {
     event.preventDefault()
     fillOutput()
 })
 
+/** This function is responsible for filling the inputs on the right side of the page,
+ *  based on the values input by the user */
 async function fillOutput() {
+
+    /** Assigning a variable for each of the inputs */
     var lado_a = parseFloat(input_data[0].value)
     var lado_b = parseFloat(input_data[1].value)
     var lado_c = parseFloat(input_data[2].value)
@@ -99,7 +119,7 @@ async function fillOutput() {
     var cor_tinta = input_data[14].value
     var titulo_projeto = input_data[15].value
  
-
+    
     const todoGeral: {
         id: number,
         lado_a: number,
@@ -137,11 +157,12 @@ async function fillOutput() {
         cor_tinta: cor_tinta,
         titulo_projeto: titulo_projeto,
     }
+    /** Calling the API to update the database with the values on the inputs */
     const responseGeral = await axios.put(`${base_url}/update/project`, todoGeral);
 
-    /** Preenchimento de quantidades e custos */
+    /** Filling the right side of the page, with quantities and costs */
 
-    /** Piso */
+    /** Floor */
     var area_piso = (lado_a) * (lado_b)
     qtd_piso.value = parseFloat(Math.round(area_piso / ((tamanho_piso / 100) * (tamanho_piso / 100))) + '')
 
@@ -155,7 +176,7 @@ async function fillOutput() {
     
     qtd_piso.value += ' Peças'
 
-    /** Telhado */
+    /** Roof */
     const tamanho1 = 0.43
     var tamanho2 = 0;
     var preco_telha = 0;
@@ -175,7 +196,7 @@ async function fillOutput() {
 
 
 
-    /** Areia e cimento */
+    /** Sand and cement */
 
     var parede = (lado_a + lado_b + lado_c + lado_d) * altura
     var largura = 0
@@ -201,7 +222,7 @@ async function fillOutput() {
     qtd_areia.value += ' m³'
     qtd_cimento.value += ' Sacos'
 
-    /** Quantidade de tijolos/blocos */
+    /** Bricks and blocks */
 
     var area_tijolo = 0
     
@@ -215,7 +236,7 @@ async function fillOutput() {
     custo_tijolo.value = 'R$' + Math.round(qtd_tijolo.value * 0.46)
     qtd_tijolo.value += ' ' + material_parede + 's'
 
-    /** Argamassa */
+    /** Grout */
 
     var argamassa_piso = 0
 
@@ -229,13 +250,13 @@ async function fillOutput() {
     custo_argamassa.value = 'R$' + Math.round(qtd_argamassa.value * 1.5)
     qtd_argamassa.value += ' kg'
 
-    /** Tinta */
+    /** Paint */
 
     qtd_tinta.value = parseFloat((parede * 2) / 10 + '')
     custo_tinta.value = 'R$' + Math.round(qtd_tinta.value * 40)
     qtd_tinta.value += ' L'
 
-    /** Custo total */
+    /** Total cost */
 
     var preco_total = 
                   parseInt(custo_areia.value.split('R$')[1])
