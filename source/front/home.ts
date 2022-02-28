0105b572a5bd09b5dfece6001ba96f0954edaaa0
talisbreda <talis.breda@outlook.com>
2022-02-28 13:57:07 -0300

    Added comments to the home.ts file

--- source/front/home.ts
+++ source/front/home.ts
@@ -1,43 +1,44 @@
 import 'regenerator-runtime/runtime';
 import axios from 'axios';
-import queryString from 'query-string';
-import { response } from 'express';
 
-/** General variables */
 const base_url = 'http://localhost:1337';
-// var idUsuario = document.cookie.split('; user=')[1]
+
+/** Splits the cookie to get the ID number of the user */
 var idUsuario = document.cookie.split('=')[1].split(';')[0]
-// console.log(document.cookie)
 
-
-/** Variables to HTML elements */
+/** Assigns variables to HTML elements */
 const header: any = document.querySelector('#header');
 const sidebar: any = document.querySelector('#sidebar');
 const topSidebar: any = document.querySelector('#topSidebar');
 const lowerSidebar: any = document.querySelector('#lowerSidebar');
 const sidebarButton: any = document.querySelector('#sidebarButton');
-let projects: any = document.querySelector('.projects');
+const projects: any = document.querySelector('.projects');
 var projectsLine: any = document.querySelector('.projectsLine');
 var projectPlaceholder: any = document.querySelectorAll('.projectPlaceholder');
 const projectName: any = document.querySelector('.projectName');
 const logoutButton: any = document.querySelector('#logout');
 const newProject: any = document.querySelector('#newProject')
 
-/** Fetch data from DB on login */
+/** This function calls the API to get all the projects related to the logged user,
+ *  creates new divs for every project fetched from the database */
 async function getProjects() {
 
     const todo = {
         id_usuario: idUsuario
     };
 
-    console.log(idUsuario);
     try {
+        /** Calls the API route to get the projects from the logged user */
         const response = await axios.post(`${base_url}/get/projects`, todo);
-        console.log(response.data);
+
+        /** Assigns the projects fetched from the database to an array */
         var projectsFetched = response.data.projects;
-        console.log(projectsFetched);
+
+        /** Creates a counter to count the amount of projects in a line */
         var count = 1;
+
         for (let i = 0; i < projectsFetched.length; i++, count++) {
+            /** Checks if the amount of projects in a line reached 3, and breaks the line if it did */
             if (count == 3) {
                 let k = document.createElement('div');
                 k.className = 'projectsLine';
@@ -45,6 +46,9 @@ async function getProjects() {
                 projectsLine = k;
                 count = 0;
             }
+
+            /** Creates a new div with the projectPlaceholder class, adds an event listener to clicks,
+             *  and uses appendChild() to push the element into the page */
             let j = document.createElement('div');
             j.className = 'projectPlaceholder';
             j.id = `projectPlaceholder${projectsFetched[i].id_projeto}`;
@@ -52,29 +56,29 @@ async function getProjects() {
                 getClickedProject(j.id);
             })
             projectsLine.appendChild(j);
-            console.log(j);
 
+            /** Creates another div with the innerProjectPlaceholder class inside the previously created
+             *  projectPlaceholder div, adds to it the title of the fetched project
+             */
             let l = document.createElement('div');
             l.className = 'innerProjectPlaceholder';
             let m: any = document.querySelector(`#projectPlaceholder${projectsFetched[i].id_projeto}`);
             m.appendChild(l);
-            console.log(l);
-
             l.innerHTML = response.data.projects[i].titulo_projeto;
         }
     } catch (error) {
-        alert('Erro ao carregar projetos');
+        alert('Error loading projects');
         console.error(error);
     }
+    /** Resets the projectPlaceholder variable to include the newly created one */
     projectPlaceholder = document.querySelectorAll('.projectPlaceholder')
 }
 getProjects();
 
-/** Button to show/hide sidebar */
+/** Adds an event listener to the sidebar button, which hides or shows the sidebar, depending on its 
+ *  current state */
 sidebarButton.addEventListener('click', async (event: Event) => {
     event.preventDefault();
-
-    projectPlaceholder = document.querySelector('.projectPlaceholder');
 
     if (sidebar.style.display == 'none') {
         sidebar.style.display = 'flex';
@@ -85,7 +89,8 @@ sidebarButton.addEventListener('click', async (event: Event) => {
     }
 });
 
-/** Logout button and user leaving page */
+/** Adds an event listener to the logout button, which deletes the user cookie and
+ * returns to the login page */
 logoutButton.addEventListener('click', async (event: Event) => {
     event.preventDefault();
 
@@ -93,7 +98,8 @@ logoutButton.addEventListener('click', async (event: Event) => {
     document.location.href = 'login.html';
 });
 
-/** Create new project */
+/** Adds an event listener to the newProject div, which creates a new project on the database,
+ *  and directs the user the the edit page */
 newProject.addEventListener('click', async (event: Event) => {
     event.preventDefault();
 
@@ -106,20 +112,25 @@ newProject.addEventListener('click', async (event: Event) => {
     document.location.href = 'edit.html'
 });
 
-/** Show project details on click */
+/** This function is called when the user clicks on a listed project, and is responsible for 
+ * directing to the edit page
+ */
 async function getClickedProject(clicked_id: string) {
+    /** Gets the ID of the clicked project */
     const project_id = clicked_id.split('projectPlaceholder')[1];
-    console.log(project_id)
 
     const todo = {
         id_projeto: project_id
     }
 
+    /** Calls the API route to fetch the project using its ID */
     const response = await axios.post(`${base_url}/get/project`, todo);
+
+    /** Assings a false value to the 'new' cookie, creates the projectID cookie and 
+     * directs to the edit page */
     document.cookie = 'new=false'
     document.cookie = `projectID=${project_id}`;
     document.location.href = 'edit.html'
-    console.log(response.data)
 }
 
 export default { getProjects };
