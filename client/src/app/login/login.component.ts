9a0bbe7be9737f1da994b61e914f0119ea14a875
talisbreda <talis.breda@outlook.com>
2022-04-26 20:37:32 -0300

    Created a redirect link to the register page

--- client/src/app/login/login.component.ts
+++ client/src/app/login/login.component.ts
@@ -1,18 +1,71 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  AfterContentChecked,
+  AfterContentInit,
+  AfterViewChecked,
+  AfterViewInit,
+  Component,
+  DoCheck,
+  Input,
+  OnChanges,
+  OnDestroy,
+  OnInit,
+  Output,
+  SimpleChanges,
+} from '@angular/core';
+import { EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent implements OnInit, OnChanges, DoCheck,
+AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit, OnDestroy {
 
-  dataSource:any = 'localhost:1337/get/all'
-  displayedColumns = ['name', 'category']
+  @Input('childToMaster') loginExists: boolean
+  @Output() destroyLogin = new EventEmitter()
 
-  constructor() {}
-
-  ngOnInit(): void {
+  login() {
+    console.log("aaaaaaa")
   }
 
+  redirectRegister() {
+    this.loginExists = false;
+    console.log(this.loginExists)
+    this.destroyLogin.emit(this.loginExists)
+  }
+
+  constructor() {
+    this.loginExists = true;
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+  ngOnInit(): void {}
+
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log("ngOnChanges")
+  }
+
+  ngDoCheck(): void {}
+
+  ngAfterContentChecked(): void {}
+
+  ngAfterViewChecked(): void {}
+
+  ngAfterContentInit(): void {}
+
+  ngAfterViewInit(): void { }
+
+  ngOnDestroy(): void {}
 }
