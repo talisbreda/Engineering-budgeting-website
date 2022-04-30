4e1823a4625b88ec556059667b772b59f7ee0981
talisbreda <talis.breda@outlook.com>
2022-04-30 18:09:11 -0300

    Connected the login component to the API

--- client/src/app/login/login.component.ts
+++ client/src/app/login/login.component.ts
@@ -5,14 +5,17 @@ import {
   AfterViewInit,
   Component,
   DoCheck,
+  ElementRef,
   Input,
   OnChanges,
   OnDestroy,
   OnInit,
   Output,
   SimpleChanges,
+  ViewChild,
 } from '@angular/core';
 import { EventEmitter } from '@angular/core';
+import axios from 'axios';
 
 @Component({
   selector: 'app-login',
@@ -22,11 +25,27 @@ import { EventEmitter } from '@angular/core';
 export class LoginComponent implements OnInit, OnChanges, DoCheck,
 AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit, OnDestroy {
 
-  @Input('destroyLogin') loginExists: boolean
+  @Input('destroyLogin') loginExists!: boolean
   @Output() destroyLogin = new EventEmitter()
+  @ViewChild('loginButton') loginButton!: ElementRef
+  @ViewChild('emailInput') email!: ElementRef
+  @ViewChild('passwordInput') password!: ElementRef
 
-  login() {
-    console.log("aaaaaaa")
+  BASE_URL: string = 'http://localhost:1337'
+
+
+  async login() {
+
+    const todo = {
+      email: this.email.nativeElement.value,
+      senha: this.password.nativeElement.value
+    }
+
+    try {
+      let response = await axios.post(`${this.BASE_URL}/login`, todo)
+    } catch (e) {
+      console.error
+    }
   }
 
   redirectRegister() {
@@ -35,9 +54,7 @@ AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit, OnDestro
     this.destroyLogin.emit(this.loginExists)
   }
 
-  constructor() {
-    this.loginExists = true;
-  }
+  constructor() {}
 
 
   ngOnInit(): void {}
