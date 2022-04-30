b83b6bd5d736d4a32d64f60e3fa14805da95da2e
talisbreda <talis.breda@outlook.com>
2022-04-30 20:10:17 -0300

    Added checking of register inputs

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -12,6 +12,7 @@ AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit {
 
   @Input('destroyRegister') registerExists: boolean
   @Output() destroyRegister = new EventEmitter()
+
   @ViewChild('nameInput') name!: ElementRef
   @ViewChild('emailInput') email!: ElementRef
   @ViewChild('passwordInput') password!: ElementRef
@@ -21,21 +22,48 @@ AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit {
 
   async register() {
 
+    const nameValue = this.name.nativeElement.value
+    const emailValue = this.email.nativeElement.value
+    const passwordValue = this.password.nativeElement.value
+    const confirmationValue = this.confirmation.nativeElement.value
+
     const todo = {
-      nome: this.name.nativeElement.value,
-      email: this.email.nativeElement.value,
-      senha: this.password.nativeElement.value
+      nome: nameValue,
+      email: emailValue,
+      senha: passwordValue
     }
 
-    console.log("aaaaaaa")
-    try {
-      let response = await axios.post(`${this.BASE_URL}/register`, todo)
-      alert(`User ${todo.nome} was successfully registered`)
-    } catch {
-      alert("Error register user")
-      console.error
+    if (nameValue.length > 0) {
+      if (emailValue.includes('@', '.com')) {
+          if (emailValue.length > 6) {
+              if (passwordValue == confirmationValue) {
+                  if (passwordValue.length >= 6) {
+                      if (passwordValue.length <= 18) {
+                        try {
+                          const response = await axios.post(`${this.BASE_URL}/register`, todo)
+                          alert(`User ${todo.nome} was successfully registered`)
+                        } catch {
+                          alert("This e-mail is already registered")
+                          console.error
+                        }
+                      } else {
+                          alert('Password is too long (maximum of 18 characters');
+                      }
+                  } else {
+                      alert('Password is too short (minimum of 6 characters)');
+                  }
+              } else {
+                  alert('Passwords do not correspond');
+              }
+          } else {
+              alert('E-mail is not valid');
+          }
+      } else {
+          alert('E-mail is not valid');
+      }
+    } else {
+      alert('Name is too short');
     }
-
   }
 
   redirectLogin() {
