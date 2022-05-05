f218f2a18701ae7a30b394b9d9971e002d47777b
talisbreda <talis.breda@outlook.com>
2022-05-05 19:58:14 -0300

    Added pointer cursor to sidebar divs and refactored part of the home component

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 export interface Section {
   name: string;
@@ -10,36 +10,9 @@ export interface Section {
   templateUrl: 'home.component.html',
   styleUrls: ['home.component.css'],
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
 
-  folders: Section[] = [
-    {
-      name: 'Photos',
-      updated: new Date('1/1/16'),
-    },
-    {
-      name: 'Recipes',
-      updated: new Date('1/17/16'),
-    },
-    {
-      name: 'Work',
-      updated: new Date('1/28/16'),
-    },
-  ];
-  notes: Section[] = [
-    {
-      name: 'Vacation Itinerary',
-      updated: new Date('2/20/16'),
-    },
-    {
-      name: 'Kitchen Remodel',
-      updated: new Date('1/18/16'),
-    },
-  ];
+  constructor() {}
 
-  constructor() {
-
-  }
-
-
+  ngOnInit(): void {}
 }
